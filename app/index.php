d7536500e160d066801a114f8f5625685524558c
acayroso <acayroso@192.168.1.5>
2017-09-07 16:19:46 +0800

    PHP and MySQL

---
+++ app/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>PHP and MySQL</title>
+    </head>
+    <body>
+        <h1>PHP and MySQL</h1>
+        
+        <h3>CRUD</h3>
+            <section>
+                <p>How to implement CRUD in PHP and MySQL</p>
+                <ul>
+                    <li>Database: employee</li>
+                    <li>Credentials: db_admin, db_password</li>
+                </ul>
+                    <?php
+
+                    ?>
+            </section>
+
+    </body>
+</html>
